4278dcfd23684e54f9ec7f7e5ddd1782641a5d0c
FirinKinuo <deals@fkinuo.ru>
2022-06-17 00:27:23 +0300

    Add: Signal surge protection when the wash post is turned on

--- mammon_gpio/__main__.py
+++ mammon_gpio/__main__.py
@@ -12,12 +12,30 @@ from mammon_gpio.gpio.pulse import PulseMoney, MoneyGPIO
 log = getLogger("main")
 
 
+def clear_if_replenishment_repeated(datetime_: datetime) -> bool:
+    replenishment = ReplenishmentHistory.get_pool_by_datetime(
+        start_datetime=datetime_.replace(microsecond=0),
+        end_datetime=datetime.now())
+    if replenishment:
+        log.info("Duplicate replenishment detected")
+        [r.delete() for r in replenishment]
+        return True
+
+    return False
+
+
 def start_loop(money_gpio: MoneyGPIO):
     while True:
         if money_gpio.money and pulse.is_timeout:
             log.info(f"Money received: {money_gpio.money}")
 
-            ReplenishmentHistory.set(datetime=datetime.now(), currency=money_gpio.money)
+            replenishment_datetime = datetime.now()
+
+            # Signal surge protection when the wash post is turned on
+            if clear_if_replenishment_repeated(datetime_=replenishment_datetime):
+                continue
+
+            ReplenishmentHistory.set(datetime=replenishment_datetime, currency=money_gpio.money)
 
             money_gpio.clear_money()
 
